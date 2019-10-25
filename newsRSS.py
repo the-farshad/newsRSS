a766754a4721e882b7dc77815dbb89c549433bb1
Farshad <Farshad.1991@gmail.com>
2019-10-25 03:01:19 +0300

    add new code, and check error

--- newsRSS.py
+++ newsRSS.py
@@ -5,13 +5,22 @@ import urllib.parse
 
 websiteURL = "https://www.kurdistan24.net/so/rsspage"
 pageRSS = requests.get(websiteURL)
-soupContent = bs4.BeautifulSoup(pageRSS.content, "lxml")
-soupLinks = soupContent.find('div', class_='rsslink')
-print(soupLinks.a['href'])
-
-
-newsFeed = feedparser.parse("https://www.kurdistan24.net/page/RSS/97.xml")
-
-
-for newsItem in newsFeed.entries:
-    print(newsItem.link)
+pageCode = pageRSS.status_code
+if (pageCode != 200):
+    print(f'RSS file not loaded, Response {pageCode} check connection and try again')
+else:
+    soupContent = bs4.BeautifulSoup(pageRSS.content, "lxml")
+    for soupLinks in soupContent.find_all('div', class_='rsslink'):
+        rssLink = soupLinks.a['href']
+        newsFeed = feedparser.parse("https://www.kurdistan24.net" + rssLink)
+        for newsItem in newsFeed.entries:
+            pageGet = requests.get(newsItem.link)
+            pageCode = pageRSS.status_code
+            print(pageCode)
+            if (pageCode != 200):
+                print(f'RSS file not loaded, Response {pageCode} check connection and try again')
+                continue
+            soupReady = bs4.BeautifulSoup(pageGet.content, 'lxml')
+            titleField = soupReady.select("div.col-xs-12.no-padding.news-reader.bg-white.no-gutter h1")
+            print(titleField)
+            dateField = soupReady.find("span" , id="registerdate")
